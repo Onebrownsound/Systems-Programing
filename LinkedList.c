e678a8beb82279455bd69872f50443260232cd72
Dom <dmodica2@pride.hofstra.edu>
2015-09-22 12:47:47 -0400

    Added printlist search list and get laste node along with nicely formating things

--- LinkedList.c
+++ LinkedList.c
@@ -7,17 +7,39 @@ struct Node{
     struct Node* next;
 };
 
+//search_list receives a root point for a list and a value to search for
+void search_list(struct Node *root,int value){
+    struct Node * starting_point = root; 
+    //travel across list until NULL is encounter, if value is encountered inform user else tell them sorry no dice :/
+    while (starting_point != NULL)
+        {
+        if (starting_point->data == value)
+        {
+            printf("The value %d was found in the list \n", value);
+            return;     
+        }
+        starting_point=starting_point->next;
+        }
+    
+    printf("The value %d was NOT found in the list \n", value);
+
+}
 void print_list(struct Node *root)
 {
     //I like to copy the root pointer just so I can't mess it up
     struct Node * starting_point = root;
+    
+    printf("Printing Your list: \n");
+    //start at starting point and traverse the list untill a NULL is encountered
     while (starting_point != NULL)
     {
-        printf("%d",starting_point->data);
+        printf("%d \n",starting_point->data);
         starting_point=starting_point->next;
     }
     
 }
+
+//construct the new node within add_node function and set appending node's next to this newly created node
 void add_node(struct Node *appending_node, int number_value)
 {
     struct Node * new_node;
@@ -25,8 +47,11 @@ void add_node(struct Node *appending_node, int number_value)
     appending_node->next=new_node;
     new_node->next=NULL;
     new_node->data=number_value;
+    printf("The value %d has been added to the linked list \n",new_node->data);
+    
 }
 
+/*a helper get_last_node function didn't even really utilize it since I kept track of tail with the current pointer within main()*/
 struct Node * get_last_node(struct Node *root)
 {
     //copy the root pointer just in case
@@ -43,12 +68,12 @@ int main(int argc, char * argv)
 {   
     //seed rng
     srand(time(NULL));
-    //root pointer
+    //basically head and tail pointer initialization
     struct Node *root;
     struct Node *current;
   
 
-    
+    //i guess this is the C version of the new() constructor in other languages....annoying and clunky
     root = (struct Node *) malloc( sizeof(struct Node) ); 
     //out of laziness I want the list to start with 10
     root->data= 10;
@@ -58,13 +83,17 @@ int main(int argc, char * argv)
     int r;
     while (index<10)
     {
-    r = rand();
+    r = rand()%10 ;
     index++;
     //send the last current node and the data into add node
     add_node(current,r);
-        
+    current=current->next;
     }
     
     print_list(root);
+    //search this list for index 10, used index++ since index is 9 at the end of the previous while loop
+    search_list(root, index++);
+    //search using a random int, didn't think this syntax would work but it does...sort of cool
+    search_list(root, rand()%10);
     
 }
