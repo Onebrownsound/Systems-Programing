64369ea55ad566e648028aabfe81fb7093c7b955
Dom <dmodica2@pride.hofstra.edu>
2015-12-07 22:37:00 -0500

    resolved i2c function call error

--- io2c_lib.c
+++ io2c_lib.c
@@ -1,23 +1,24 @@
 //Author Dom Modica
 //Purpose Complete Final Project for servo library
-#include <bcm2835.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/ioctl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
 #include <linux/i2c-dev.h>
 #include <fcntl.h>
-
 //Setup macros for Register constants from PYTHON lib
-#define  __MODE1 				0x00
-#define  __MODE2 				0x01
-#define  __SUBADR1 			0x02
-#define  __SUBADR2 			0x03
-#define  __SUBADR3		   0x04
-#define  __PRESCALE 			0xFE
-#define  __LED0_ON_L  		0x06
-#define  __LED0_ON_H 		0x07
-#define  __LED0_OFF_L 		0x08
+#define  __MODE1           0x00
+#define  __MODE2	   0x01
+#define  __SUBADR1     	   0x02
+#define  __SUBADR2	   0x03
+#define  __SUBADR3         0x04
+#define  __PRESCALE 	   0xFE
+#define  __LED0_ON_L  	   0x06
+#define  __LED0_ON_H   	   0x07
+#define  __LED0_OFF_L 	   0x08
 #define  __LED0_OFF_H      0x09
 #define  __ALL_LED_ON_L    0xFA
 #define  __ALL_LED_ON_H    0xFB
@@ -38,12 +39,10 @@
 
 
 int setup() //setup tells the kernel i2c driver which i2c to use
-{
+{	printf("Attempting to setup i2c file");
 	int file;
 	int adapter_nr = 1; /* needs to be looked up in /dev/i2c-NUMBERISHERE for me it is 1 */
 	char *filename = "/dev/i2c-1";
-
-	snprintf(filename, 19, "/dev/i2c-%d", adapter_nr);
 	file = open(filename, O_RDWR);
 	if (file < 0) {
 	 /* ERROR HANDLING;*/
@@ -64,19 +63,19 @@ int setup() //setup tells the kernel i2c driver which i2c to use
 
 
 
-int main(){
+int main (int argc, char * argv [] )
+{
+printf("Hello");
 int file_number;
 file_number=setup();
+printf("The file_number is  %i /n",file_number);
 while(1){
 //write byte data to (file,register,value)
-i2c_smbus_write_byte_data(file_number,__LED0_ON_L+4 * CHANNEL, on & 0xFF)
-i2c_smbus_write_byte_data(file_number,__LED0_ON_H+4 * CHANNEL, on >> 8)
-i2c_smbus_write_byte_data(file_number,__LED0_OFF_L+4 * CHANNEL, off & 0xFF)
-i2c_smbus_write_byte_data(file_number,__LED0_OFF_H+4 * CHANNEL, off >> 8)
+i2c_smbus_write_byte_data (file_number,__LED0_ON_L, SERVO_MIN & 0xFF);
+i2c_smbus_write_byte_data(file_number,__LED0_ON_H, SERVO_MIN >> 8);
+i2c_smbus_write_byte_data(file_number,__LED0_OFF_L, SERVO_MAX & 0xFF);
+i2c_smbus_write_byte_data(file_number,__LED0_OFF_H, SERVO_MAX >> 8);
 }
-  
-
-  
 
 return 0;
 }
